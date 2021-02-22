20f26c01090bf5ad731ec327c98e7b6d43de91f0
nicolas blum <nblumferracci@gmail.com>
2021-02-22 16:40:10 +0100

    set bird class and whobble

---
+++ bird.js
@@ -0,0 +1,34 @@
+class Bird {
+  constructor() {
+    this.x = 150;
+    this.y = 200;
+    this.velocityY = 0;
+    this.width = 20;
+    this.height = 20;
+    this.weight = 1;
+  }
+  updateAnimation() {
+    let curve = Math.sin(angle) * 5;
+    if (this.y > canvas.height - this.height * 3 + curve) {
+      this.y = canvas.height - this.height * 3 + curve;
+      this.velocityY = 0;
+    } else {
+      this.velocityY += this.weight;
+      this.velocityY *= 0.9;
+      this.y += this.velocityY;
+    }
+    if (this.y < 0 + this.height) {
+      this.y = 0 + this.height;
+      this.velocityY = 0;
+    }
+    if (spacePressed && this.y > this.height * 3) this.flap();
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  flap() {
+    this.velocityY -= 2;
+  }
+}
+const bird = new Bird();
